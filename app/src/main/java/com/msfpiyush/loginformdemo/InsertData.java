26344fa0b65c9b9c995057ce3c003a6e58d7b906
adminsy5 <piyush@cbigujarat.onmicrosoft.com>
2023-01-23 07:08:51 +0530

    Debug the code v1.0

--- app/src/main/java/com/msfpiyush/loginformdemo/InsertData.java
+++ app/src/main/java/com/msfpiyush/loginformdemo/InsertData.java
@@ -6,6 +6,7 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -17,6 +18,7 @@ import androidx.activity.result.ActivityResultCallback;
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.gms.tasks.OnSuccessListener;
@@ -25,13 +27,19 @@ import com.google.firebase.storage.OnProgressListener;
 import com.google.firebase.storage.StorageReference;
 import com.google.firebase.storage.UploadTask;
 import com.karumi.dexter.Dexter;
+import com.karumi.dexter.DexterBuilder;
+import com.karumi.dexter.MultiplePermissionsReport;
 import com.karumi.dexter.PermissionToken;
 import com.karumi.dexter.listener.PermissionDeniedResponse;
 import com.karumi.dexter.listener.PermissionGrantedResponse;
 import com.karumi.dexter.listener.PermissionRequest;
+import com.karumi.dexter.listener.multi.MultiplePermissionsListener;
+import com.karumi.dexter.listener.single.CompositePermissionListener;
+import com.karumi.dexter.listener.single.DialogOnDeniedPermissionListener;
 import com.karumi.dexter.listener.single.PermissionListener;
 
 import java.io.InputStream;
+import java.util.List;
 
 public class InsertData extends AppCompatActivity {
     EditText EditName,EditCourse,EditEmail;
@@ -54,24 +62,48 @@ public class InsertData extends AppCompatActivity {
         BrowseButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Dexter.withContext(getApplicationContext())
-                        .withPermission(Manifest.permission.READ_EXTERNAL_STORAGE)
-                        .withListener(new PermissionListener() {
-                            @Override
-                            public void onPermissionGranted(PermissionGrantedResponse permissionGrantedResponse) {
-                                Toast.makeText(InsertData.this, "working", Toast.LENGTH_SHORT).show();
-                            }
-
-                            @Override
-                            public void onPermissionDenied(PermissionDeniedResponse permissionDeniedResponse) {
-                                Toast.makeText(InsertData.this, "error", Toast.LENGTH_SHORT).show();
-                            }
-
-                            @Override
-                            public void onPermissionRationaleShouldBeShown(PermissionRequest permissionRequest, PermissionToken permissionToken) {
-                                permissionToken.continuePermissionRequest();
-                            }
-                        }).check();
+                Toast.makeText(InsertData.this, "Try", Toast.LENGTH_SHORT).show();
+                requestPermissionsListener();
+//                Dexter.withContext(InsertData.this)
+//                        .withPermission(Manifest.permission.READ_EXTERNAL_STORAGE)
+//                        .withListener(new CompositePermissionListener(new PermissionListener() {
+//                            @Override
+//                            public void onPermissionGranted(PermissionGrantedResponse permissionGrantedResponse) {
+//                                Toast.makeText(InsertData.this, "working", Toast.LENGTH_SHORT).show();
+//                            }
+//
+//                            @Override
+//                            public void onPermissionDenied(PermissionDeniedResponse permissionDeniedResponse) {
+//                                Toast.makeText(InsertData.this, "Error!", Toast.LENGTH_SHORT).show();
+//                            }
+//
+//                            @Override
+//                            public void onPermissionRationaleShouldBeShown(PermissionRequest permissionRequest, PermissionToken permissionToken) {
+//                                permissionToken.continuePermissionRequest();
+//                            }
+//                        }, DialogOnDeniedPermissionListener.Builder
+//                                .withContext(InsertData.this).withTitle("Read permission")
+//                                .withMessage("Reading the sdcard is required").withButtonText(android.R.string.yes)
+//                                .build())).check();
+                //.check();
+//                Dexter.withContext(getApplicationContext())
+//                        .withPermission(Manifest.permission.READ_EXTERNAL_STORAGE)
+//                        .withListener(new PermissionListener() {
+//                            @Override
+//                            public void onPermissionGranted(PermissionGrantedResponse permissionGrantedResponse) {
+//                                Toast.makeText(InsertData.this, "working", Toast.LENGTH_SHORT).show();
+//                            }
+//
+//                            @Override
+//                            public void onPermissionDenied(PermissionDeniedResponse permissionDeniedResponse) {
+//                                Toast.makeText(InsertData.this, "error", Toast.LENGTH_SHORT).show();
+//                            }
+//
+//                            @Override
+//                            public void onPermissionRationaleShouldBeShown(PermissionRequest permissionRequest, PermissionToken permissionToken) {
+//                                permissionToken.continuePermissionRequest();
+//                            }
+//                        }).check();
             }
         });
         activityResultLauncher=registerForActivityResult(new ActivityResultContracts.StartActivityForResult(), new ActivityResultCallback<ActivityResult>() {
@@ -127,6 +159,70 @@ public class InsertData extends AppCompatActivity {
             }
         });
     }
+
+    private void requestPermissionsListener() {
+        Dexter.withContext(this)
+                // below line is use to request the number of permissions which are required in our app.
+                .withPermissions(Manifest.permission.CAMERA,
+                        // below is the list of permissions
+                        Manifest.permission.ACCESS_FINE_LOCATION,
+                        Manifest.permission.READ_CONTACTS)
+                // after adding permissions we are calling an with listener method.
+                .withListener(new MultiplePermissionsListener() {
+                    @Override
+                    public void onPermissionsChecked(MultiplePermissionsReport multiplePermissionsReport) {
+                        // this method is called when all permissions are granted
+                        if (multiplePermissionsReport.areAllPermissionsGranted()) {
+                            // do you work now
+                            Toast.makeText(InsertData.this, "All the permissions are granted..", Toast.LENGTH_SHORT).show();
+                        }
+                        // check for permanent denial of any permission
+                        if (multiplePermissionsReport.isAnyPermissionPermanentlyDenied()) {
+                            // permission is denied permanently, we will show user a dialog message.
+//                            showSettingsDialog();
+                        }
+                    }
+
+                    @Override
+                    public void onPermissionRationaleShouldBeShown(List<PermissionRequest> list, PermissionToken permissionToken) {
+                        // this method is called when user grants some permission and denies some of them.
+                        permissionToken.continuePermissionRequest();
+                    }
+                }).withErrorListener(error -> {
+                    // we are displaying a toast message for error message.
+                    Toast.makeText(getApplicationContext(), "Error occurred! ", Toast.LENGTH_SHORT).show();
+                })
+                // below line is use to run the permissions on same thread and to check the permissions
+                .onSameThread().check();
+    }
+
+    // below is the shoe setting dialog method which is use to display a dialogue message.
+//    private void showSettingsDialog() {
+//        // we are displaying an alert dialog for permissions
+//        AlertDialog.Builder builder = new AlertDialog.Builder(InsertData.this);
+//
+//        // below line is the title for our alert dialog.
+//        builder.setTitle("Need Permissions");
+//
+//        // below line is our message for our dialog
+//        builder.setMessage("This app needs permission to use this feature. You can grant them in app settings.");
+//        builder.setPositiveButton("GOTO SETTINGS", (dialog, which) -> {
+//            // this method is called on click on positive button and on clicking shit button
+//            // we are redirecting our user from our app to the settings page of our app.
+//            dialog.cancel();
+//            // below is the intent from which we are redirecting our user.
+//            Intent intent = new Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
+//            Uri uri = Uri.fromParts("package", getPackageName(), null);
+//            intent.setData(uri);
+//            startActivityForResult(intent, 101);
+//        });
+//        builder.setNegativeButton("Cancel", (dialog, which) -> {
+//            // this method is called when user click on negative button.
+//            dialog.cancel();
+//        });
+//        // below line is used to display our dialog
+//        builder.show();
+//    }
 
 //    @Override
 //    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data)
